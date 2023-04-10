6653bbf87d1fd2701e66c74b6e64684539c194ff
Jester0027 <paul.ned@outlook.com>
2023-04-10 11:57:42 +0200

    initial

---
+++ Role.cs
@@ -0,0 +1,88 @@
+﻿namespace EnumsTest;
+
+public enum Role
+{
+    [Data(name: "Administrator", claim: RoleClaims.Administrator)]
+    Admin = 0,
+
+    [Data(name: "User", claim: RoleClaims.User)]
+    User = 1,
+
+    [Data(name: "Guest", claim: RoleClaims.Guest)]
+    Guest = 2,
+}
+
+public static class RoleClaims
+{
+    public const string Administrator = "com.example.roles.administrator";
+    public const string User = "com.example.roles.user";
+    public const string Guest = "com.example.roles.guest";
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public sealed class DataAttribute : Attribute
+{
+    public string Name { get; set; }
+    public string Claim { get; init; }
+
+    public DataAttribute(string name, string claim)
+    {
+        Name = name;
+        Claim = claim;
+    }
+}
+
+public static class RoleUtils
+{
+    public static Role FromClaim(string claim)
+    {
+        return Enum
+            .GetValues(typeof(Role))
+            .Cast<Role>()
+            .Single(role => role.GetClaim() == claim);
+    }
+}
+
+public static class RoleExtensions
+{
+    public static string GetName(this Role role)
+    {
+        var attribute = role.GetAttribute<DataAttribute>();
+
+        return attribute.Name;
+    }
+
+    public static string GetClaim(this Role role)
+    {
+        var attribute = role.GetAttribute<DataAttribute>();
+
+        return attribute.Claim;
+    }
+}
+
+public static class EnumExtensions
+{
+    public static TAttribute GetAttribute<TAttribute>(this Enum value)
+        where TAttribute : Attribute
+    {
+        var type = value.GetType();
+        var name = Enum.GetName(type, value);
+        if (name is null)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), value, null);
+        }
+
+        var field = type.GetField(name);
+        if (field is null)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), value, null);
+        }
+
+        if (Attribute.GetCustomAttribute(field, typeof(TAttribute)) is not TAttribute attribute)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), value, null);
+        }
+
+        return attribute;
+    }
+}
---
+++ RolesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnumsTest;
+
+[ApiController]
+[Route("roles")]
+public class RolesController : ControllerBase
+{
+    private sealed record RoleResponse(string Name, string? Claim);
+
+    /// <summary>
+    /// Returns all roles and their claims
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var roles = Enum.GetValues(typeof(Role))
+            .Cast<Role>()
+            .Select(role => new RoleResponse(role.GetName(), role.GetClaim()))
+            .ToList();
+        return Ok(roles);
+    }
+
+    public sealed record RoleRequest(string Claim);
+
+    /// <summary>
+    /// Get a role from a claim
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public IActionResult FromClaim([FromBody] RoleRequest request)
+    {
+        var role = RoleUtils.FromClaim(request.Claim);
+        return Ok(new RoleResponse(role.GetName(), role.GetClaim()));
+    }
+}
